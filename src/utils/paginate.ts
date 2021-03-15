d75388ca8ac1bc4f6811809a7cf39ff19f1bd686
Madhav Anand <madhav1004@icloud.com>
2021-03-15 09:58:04 +0530

    now using ts-node-dev instead of nodemon ts-node combo

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import { BlockInterface } from "../libs/block";
 
 const paginate = async (
   numberPerPage: number,
   page: number,
   Model: mongoose.Model<any>,
-): Promise<Array<BlockInterface>> => {
-  const blocks = await Model.find()
+): Promise<Array<any>> => {
+  const blocks = await Model.find({})
     .skip(numberPerPage * (page - 1))
     .limit(numberPerPage)
     .exec();
