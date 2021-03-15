3cde52d010838eead902859305bf2de7a6bccc41
Madhav Anand <madhav1004@icloud.com>
2021-03-15 20:02:57 +0530

    initialized graphql in seed-node

--- helpers/seed-node/pages/api/graphql.js
+++ helpers/seed-node/pages/api/graphql.js
@@ -1,26 +1,122 @@
-import { ApolloServer, gql } from 'apollo-server-micro'
+import { ApolloServer, gql } from "apollo-server-micro";
+import mongoose, { Schema } from "mongoose";
+import GraphQLJSON, { GraphQLJSONObject } from "graphql-type-json";
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongo db"))
+  .catch((err) => console.log(`Error in mongo db ${err}`));
+
+const NodeSchema = new mongoose.Schema({
+  address: {
+    required: true,
+    type: String,
+  },
+  length: {
+    required: true,
+    type: Number,
+  },
+  lastBlock: {
+    index: {
+      required: true,
+      type: Number,
+    },
+    data: {
+      required: true,
+      type: Schema.Types.Mixed,
+    },
+    nonce: {
+      required: true,
+      type: Number,
+    },
+    timestamp: {
+      required: true,
+      type: Number,
+    },
+    prevHash: {
+      required: true,
+      type: String,
+    },
+    difficulty: {
+      required: true,
+      type: Number,
+    },
+  },
+});
+
+const Node = mongoose.models.Nodes || mongoose.model("Nodes", NodeSchema);
 
 const typeDefs = gql`
-    type Query {
-        me: String!
-    }
+  scalar JSON
+
+  type Query {
+    me: String!
+  }
+
+  type Block {
+    index: Int!
+    data: JSON!
+    nonce: Int!
+    timestamp: Float!
+    prevHash: String!
+    difficulty: Int!
+  }
+
+  input BlockInput {
+    index: Int!
+    data: JSON!
+    nonce: Int!
+    timestamp: Float!
+    prevHash: String!
+    difficulty: Int!
+  }
+
+  input NodeInput {
+    address: String!
+    length: Int!
+    lastBlock: BlockInput!
+  }
+
+  type Node {
+    address: String!
+    length: Int!
+    lastBlock: Block!
+  }
+
+  type Mutation {
+    addNode(input: NodeInput!): Node!
+  }
 `;
 
 const resolvers = {
+  JSON: GraphQLJSONObject,
   Query: {
-    me: ()=> "Hello hello hello seed node kiyosaki here."
-  }
-}
+    me: () => "Hello hello hello seed node kiyosaki here.",
+  },
+  Mutation: {
+    addNode: (_, { input: { address, length, lastBlock } }) => {
+      return {
+        address,
+        length,
+        lastBlock,
+      };
+    },
+  },
+};
 
 const apolloServer = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
-export default apolloServer.createHandler({ path: '/api/graphql' })
+export default apolloServer.createHandler({ path: "/api/graphql" });
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
